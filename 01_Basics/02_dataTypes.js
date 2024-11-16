9d42ae7b310721ae4c944653347b6bbe2bed8405
Dreamingisart <158299221+Dreamingisart@users.noreply.github.com>
2024-11-16 19:00:36 +0000

    data types

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -2,3 +2,19 @@
 
 // alert (3 + 3) // We are using nodejs not browser
 // code readabilty should be high
+
+// Data types in JS
+/*
+number => range is from 2 to the power 53
+big int
+string
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+object => 
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+ 
