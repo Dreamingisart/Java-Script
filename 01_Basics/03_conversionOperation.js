84aa71868ed9368bdbb3b529a58da525461792fb
Dreamingisart <158299221+Dreamingisart@users.noreply.github.com>
2024-11-19 15:40:10 +0000

    Conversion of Data Types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -2,10 +2,14 @@
 // "33abc" => NaN (Not a Number)
 // true => 1; false => 0
 
-let isLoggedIn = 1;
+let /0 = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false;
 // "abcd" => true;
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
